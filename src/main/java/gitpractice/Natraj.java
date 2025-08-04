2eb4c605ce91bcc5fcf6375aa8f22b4133ca37d0
Leoprasann <leoprasann33@gmail.com>
2025-08-04 14:36:09 +0530

    Added by Prasanna Kumar

---
+++ src/main/java/gitpractice/Natraj.java
@@ -0,0 +1,13 @@
+package gitpractice;
+
+public class Natraj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Natraj";
+		String s2 = s1.substring(0, 2);
+		s1 = s2;
+		System.out.println(s1);
+	}
+
+}
